f6b5bec81f1cdf818b27bec31390ce615a47e10a
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-04-19 12:50:18 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap{
+static public  void main (String [] args)
+{
+int a = 10, b =12;
+System.out.println("numbers before swapping" +a + " " +b);
+int temp = a;
+a = b ;
+b = temp;
+System.out.println("numbers after swapping " +a + " " +b);
+
+}
+}
