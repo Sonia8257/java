7916f8e689c53b6037cd3b2e23ca183abca64304
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-04-19 12:53:53 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,14 @@
+public class Datatypes{
+public static void main (String [] args)
+{
+int a= 10101;
+float b=2256;
+char c = 'S';
+String d = "123AB";  
+
+System.out.println(a);
+System.out.println(d);
+System.out.println(c);
+System.out.println(b);
+}
+}
