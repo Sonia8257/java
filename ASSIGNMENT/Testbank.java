5c032bbdf842adcc414a14fbbd1620d251a07ce9
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-05-10 10:04:43 +0530

    Add files via upload

---
+++ ASSIGNMENT/Testbank.java
@@ -0,0 +1,33 @@
+class Bank
+{
+void interest()
+{
+System.out.println("  WELCOME TO BANK PORTAL   ");
+}
+}
+class Axis extends Bank
+{
+void interest()
+{
+System.out.println("Welcome Axis to bank portal");
+}
+}
+class Icici extends Bank
+{
+void interest()
+{
+System.out.println("Welcome Icici to bank portal");
+}
+}
+public class Testbank
+{
+public static void main (String [] args)
+{
+
+Icici i = new Icici();
+i.interest();
+Axis a = new Axis();
+a.interest();
+}
+}
+
