9cbe4dae5532b0f44263797daa04fb43e65a5b31
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-05-20 10:08:23 +0530

    Add files via upload

---
+++ DAILY ASSIGNMENT/Stringbuffer.java
@@ -0,0 +1,10 @@
+class Stringbuffer
+{
+public static void main (String [] args)
+{
+StringBuffer s = new StringBuffer("Simbha");
+s.substring(0,2);
+
+System.out.println("Substring is :  "+s);
+}
+}
