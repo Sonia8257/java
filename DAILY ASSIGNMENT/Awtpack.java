b5d8b208c3d0b62a862edd4ffdb2ff469e7a5ea6
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-05-26 12:02:40 +0530

    Add files via upload

---
+++ DAILY ASSIGNMENT/Awtpack.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+public class Awtpack {
+
+	public static void main(String[] args) {
+		Frame f= new Frame();
+		Label l = new Label("ENTER YOUR EMAILID:");
+		f.add(l);
+		f.setVisible(true);
+		Button b= new Button("login");
+		f.add(b);
+		 
+
+	}
+
+}
