636bf21ce1ccae78c6f6d778536cdd8df8d31f91
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-05-26 13:44:40 +0530

    Create  Exceptionclass.java

---
+++ LAB ACTIVITY/Exceptionclass.java
@@ -0,0 +1,33 @@
+package assignment;
+import java.util.Scanner;
+public class Exceptionclass {
+	void add(int a , int b)
+	{
+		int sum = a+b;
+		if (sum <100)
+		{
+			System.out.println("ANS :" +sum);
+		}
+		else
+		{
+		throw new ArithmeticException("Sum is greater thean 100");
+		
+			
+		}
+				}
+
+
+	
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.println("ENTER ANY TWO NUMBERS ;");
+int a = sc.nextInt();
+int b = sc.nextInt();
+Exceptionclass e = new Exceptionclass ();
+e.add(a,b);
+
+
+	}
+}
+
