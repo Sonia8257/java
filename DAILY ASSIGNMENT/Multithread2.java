25022b248688e21357593cd075bf7f27c62a12bb
Sonia8257 <103440123+Sonia8257@users.noreply.github.com>
2022-06-06 09:16:08 +0530

    Add files via upload

---
+++ DAILY ASSIGNMENT/Multithread2.java
@@ -0,0 +1,23 @@
+public class Multithread2 extends Thread {
+	public void run()
+	{
+		System.out.println("Threads invoke..");
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		 Multithread2 m=new  Multithread2();
+		 
+		 m.start();
+		 m.setName("process");
+		 
+		 System.out.println("The name of thread is:"+m.getName());
+		 System.out.println("The name of thread is:"+m.getId());
+		 System.out.println("The thread m priority is:"+m.getPriority());
+		
+		
+	}	
+		
+	}
