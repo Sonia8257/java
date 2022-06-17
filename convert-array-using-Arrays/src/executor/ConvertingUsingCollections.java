a3c5fa986d8630b81604b380f8df2516da4d1f6c
sonia <soniapragalathan855@gmail.com>
2022-06-17 12:41:40 +0530

    commit

---
+++ convert-array-using-Arrays/src/executor/ConvertingUsingCollections.java
@@ -0,0 +1,33 @@
+//converting array into arraylist using Collections
+package executor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ConvertingUsingCollections {
+
+	public static void main(String[] args) {
+		
+		//creating a array of type String and intializing on compile time
+				String stud[] = {"Shinchan","kasama","boo","masaov"};
+				
+				//Creating ArrayList
+				ArrayList<String> s= new ArrayList<String>();
+				
+				//converting array into arraylist using Collections
+				Collections.addAll(s,stud);
+				
+				//adding elements in arraylist
+				s.add("michchi");
+				System.out.println("ArraysList is ");
+				
+				
+				//using advanced for loop for iterating throught elements
+				for(String b:s)
+				{
+					System.out.println(b);
+				}
+				
+	}
+
+}
